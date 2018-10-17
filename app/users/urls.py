bfae5ab927b2e8cfc493459e51a2c4380f2906d8
Piyush <pysingh09@gmail.com>
2018-10-17 19:42:58 +0530

    Created url for users api

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name='users'
 
 urlpatterns = [
+    url(r'^user/changepassword$',views.ChangePassword.as_view()),
     url(r'^user/login$',views.LoginApi.as_view()),
-    url(r'^user/(?P<user_id>[0-9]+)$',views.UserApi.as_view()),
+    url(r'^user/edit/(?P<user_id>[0-9]+)$',views.UserApi.as_view()),
+    url(r'^user/disable/(?P<user_id>[0-9]+)$',views.UserApi.as_view()),
     url(r'^user/create$',views.UserApi.as_view()),
+    url(r'^user/forgotpassword$',views.ChangePassword.as_view()),
 
 ]
