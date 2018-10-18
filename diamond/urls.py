112260e40ead7327ca22b4d3b4cd9997d48df94d
Piyush <pysingh09@gmail.com>
2018-10-18 20:15:07 +0530

    included project api

--- diamond/urls.py
+++ diamond/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^', include('app.users.urls')),
+    url(r'^', include('app.projects.urls')),
     # path('admin/', admin.site.urls),
 ]
