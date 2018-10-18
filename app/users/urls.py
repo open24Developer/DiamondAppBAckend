a0d3839df0b073185e780e84542079ae458b70f9
Piyush <pysingh09@gmail.com>
2018-10-18 12:50:29 +0530

    Updated forgotpassword url

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name='users'
 
 urlpatterns = [
+    url(r'^user/forgotpassword$',views.ForGotPassword.as_view()),
     url(r'^user/changepassword$',views.ChangePassword.as_view()),
     url(r'^user/login$',views.LoginApi.as_view()),
     url(r'^user/edit/(?P<user_id>[0-9]+)$',views.UserApi.as_view()),
     url(r'^user/disable/(?P<user_id>[0-9]+)$',views.UserApi.as_view()),
     url(r'^user/create$',views.UserApi.as_view()),
-    url(r'^user/forgotpassword$',views.ChangePassword.as_view()),
+    
 
 ]
